4575041f8e5ef1615d4bb778ce85753644666e53
LastVoyager <maximvb89@gmail.com>
2024-04-11 12:45:43 +0300

    11.04.

--- src/Components/GoodDetail/GoodDetail.js
+++ src/Components/GoodDetail/GoodDetail.js
@@ -9,14 +9,14 @@ function GoodDetail() {
   
   //Redux
   const goods = useSelector(state => state.cart.goods);
-  const info = goods.find((info) => info.id === params.goodId);
+  const goodDetails = goods.find((info) => info.id === Number(params.goodId));
 
   console.log(goods); //result: SHOWS ARRAY
-  console.log(info);  //result: UNDEFINED
+  console.log(goodDetails);  //result: UNDEFINED
 
   const dispatch = useDispatch();
-  const addToCart = (info) => {
-    dispatch(goodsActions.addToCart(info));
+  const addToCart = (goodDetails) => {
+    dispatch(goodsActions.addToCart(goodDetails));
   };
 
   return (
@@ -24,8 +24,10 @@ function GoodDetail() {
       <Route path={match.path} exact>
         <div className="main-container">
           <section className="main-proposition">
-            <h1>{goods[0].title}</h1> {/* //result: FIRST OF ALL SHOWING VALID VALUE AFTER PAGE RELOADING it CRASHES */}
-            <p></p>
+            <img src={`${process.env.REACT_APP_SERVER_URL}/${goodDetails.image}`} alt={goodDetails.title}/>
+            <h1>{goodDetails.title}</h1> {/* //result: FIRST OF ALL SHOWING VALID VALUE AFTER PAGE RELOADING it CRASHES */}
+            <p>{goodDetails.price}</p>
+            <p>{goodDetails.info}</p>
             <button className="button" onClick={addToCart}>Add to cart</button>
              {/*  <Link className="button" to={`/main/${props.info.id}`}>Show more info</Link> */}
             <Link className="btn--flat" to={`${match.url}/comments`}></Link>
